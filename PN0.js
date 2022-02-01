80177e67d95d1091d4ea96213f1f294ce62d81d9
Mrityunjay vyas <58501800+Mrityunjay383@users.noreply.github.com>
2022-02-01 14:29:06 +0530

    Create PN0.js

---
+++ PN0.js
@@ -0,0 +1,9 @@
+let num = -10;
+
+if(num % 2 == 0){
+    console.log("The number is Zero");
+}else if(num < 0){
+    console.log("The number is Negative");
+}else{
+    console.log("The number is Positive");
+}
