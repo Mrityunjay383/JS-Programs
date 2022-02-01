d5a55f56f00224727c17388974a6e2159a0636b4
Mrityunjay vyas <58501800+Mrityunjay383@users.noreply.github.com>
2022-02-01 14:21:46 +0530

    Create sum&avgOf2nums.js

---
+++ sum&avgOf2nums.js
@@ -0,0 +1,9 @@
+let a = 5, b = 10; //Change variable value here
+
+
+let sum = a+b;
+let avg = sum/2;
+
+//Priting values
+console.log(`Sum of two numbers is: ${sum}`);
+console.log(`Average of two numbers is: ${avg}`);
